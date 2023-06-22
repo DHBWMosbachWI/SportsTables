f02d9de58f58414ac9f57c7c7471a76a71a692b2
jule <a@test.com>
2023-06-22 19:32:42 +0200

    volleyball fix bundesliga problem

--- volleyball/web_scraping.py
+++ volleyball/web_scraping.py
@@ -45,33 +45,47 @@ def web_table_to_dataframe(web_table):
 
 
 def check_tables(result):
-    if type(result) == list: #result can be single df or a list of df
+    if type(result) == list:  # result can be single df or a list of df
         new_result = []
         for table in range(len(result)):
-            if result[table].columns.nlevels > 1: # are there more than one header level?
-                n_level = result[table].columns.nlevels-1
+            if result[table].columns.nlevels > 1:  # are there more than one header level?
+                n_level = result[table].columns.nlevels - 1
                 for index in range(n_level):
-                    result[table].columns = result[table].columns.droplevel(0) # remove first header until only one is left
+                    result[table].columns = result[table].columns.droplevel(
+                        0)  # remove first header until only one is left
             columns = list(result[table].columns)
             new_result.append(result[table].loc[result[table][columns[0]] != columns[0]])
             # are headers repeated as rows? Remove these!
     else:
         if result.columns.nlevels > 1:
-            n_level = result.columns.nlevels-1
+            n_level = result.columns.nlevels - 1
             for index in range(n_level):
-                result.columns = result.columns.droplevel(0) # drop first layer
+                result.columns = result.columns.droplevel(0)  # drop first layer
         columns = list(result.columns)
         new_result = result.loc[result[columns[0]] != columns[0]]
-        if result.columns[0] == 0: # anomalies in team stats
+        if result.columns[0] == 0:  # anomalies in team stats
             result.columns = result.iloc[3]
             new_result = result.iloc[4:]
     return new_result
+
 
 def get_volleyball_league_scores_bundesliga_men(year, driver):
     url = f'https://www.volleyball-bundesliga.de/cms/home/1_bundesliga_maenner/archiv/statistikrankings/saison_{year}_{year + 1 - 2000}.xhtml'
     driver.get(url)
     time.sleep(5)
+    while True:
+        try:
+            button = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[3]/button[1]")
+            button.click()
+
+        except:
+            break
     try:
+        dropdown = driver.find_element(By.XPATH,
+                                       "/html/body/div[1]/div[4]/div/div/div[2]/div[1]/div[2]/form/div/div[2]/div/div/div[1]/select")
+        select = Select(dropdown)
+        select.select_by_visible_text("100")
+        time.sleep(7)
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
         tables = soup.find_all('table')
@@ -89,12 +103,24 @@ def get_volleyball_league_scores_bundesliga_men(year, driver):
         print("not possible")
         print(e)
         pass
+
 
 def get_volleyball_league_scores_bundesliga_women(year, driver):
     url = f'https://www.volleyball-bundesliga.de/cms/home/1_bundesliga_frauen/archiv/statistikrankings/saison_{year}_{year + 1 - 2000}.xhtml'
     driver.get(url)
     time.sleep(5)
+    while True:
+        try:
+            button = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[3]/button[1]")
+            button.click()
+        except:
+            break
     try:
+        dropdown = driver.find_element(By.XPATH,
+                                       "/html/body/div[1]/div[4]/div/div/div[2]/div[1]/div[2]/form/div/div[2]/div/div/div[1]/select")
+        select = Select(dropdown)
+        select.select_by_visible_text("100")
+        time.sleep(7)
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
         tables = soup.find_all('table')
@@ -112,6 +138,7 @@ def get_volleyball_league_scores_bundesliga_women(year, driver):
         print("not possible")
         print(e)
         pass
+
 
 def safe_csv(df, filename):
     try:
@@ -120,9 +147,11 @@ def safe_csv(df, filename):
     except Exception as e:
         print('could not safe csv file')
         print(e)
+
+
 def get_championsleague_women(year, driver):
-    try: # first load the website of the given year
-        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1262') # contains table with all years
+    try:  # first load the website of the given year
+        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1262')  # contains table with all years
         link = driver.find_element(By.PARTIAL_LINK_TEXT, f'{year}').get_attribute('href')
         driver.get(link)
         # it's not possible to use the click() function, therefore you have to get the url and use driver.get()
@@ -134,9 +163,10 @@ def get_championsleague_women(year, driver):
     get_team_stats(year, driver, league)
     get_player_stats(year, driver, league)
 
+
 def get_championsleague_men(year, driver):
     try:
-        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1259') # contains table with all years
+        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1259')  # contains table with all years
         link = driver.find_element(By.PARTIAL_LINK_TEXT, f'{year}').get_attribute('href')
         driver.get(link)
         # it's not possible to use the click() function, therefore you have to get the url and use driver.get()
@@ -147,45 +177,49 @@ def get_championsleague_men(year, driver):
     get_team_standings(year, driver, league)
     get_team_stats(year, driver, league)
     get_player_stats(year, driver, league)
+
+
 def get_cev_cup_men(year, driver):
-    if year < 2012: # inconsistent writing of the year
-        year = year-1
+    if year < 2012:  # inconsistent writing of the year
+        year = year - 1
     try:
-        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1260') # contains table with all years
+        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1260')  # contains table with all years
         link = driver.find_element(By.PARTIAL_LINK_TEXT, f'{year}').get_attribute('href')
         driver.get(link)
         # it's not possible to use the click() function, therefore you have to get the url and use driver.get()
     except Exception as e:
         print('could not load given year!')
         print(e)
-    if year < 2012: # redo year so filename is correct
-        year = year+1
+    if year < 2012:  # redo year so filename is correct
+        year = year + 1
     league = f'volleyball_cev_cup_men'
     get_team_standings(year, driver, league)
     get_team_stats(year, driver, league)
     get_player_stats(year, driver, league)
 
+
 def get_cev_cup_women(year, driver):
-    if year < 2012: # inconsistent writing of the year
-        year = year-1
+    if year < 2012:  # inconsistent writing of the year
+        year = year - 1
     try:
-        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=5') # contains table with all years
+        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=5')  # contains table with all years
         link = driver.find_element(By.PARTIAL_LINK_TEXT, f'{year}').get_attribute('href')
         driver.get(link)
         # it's not possible to use the click() function, therefore you have to get the url and use driver.get()
     except Exception as e:
         print('could not load given year!')
         print(e)
-    if year < 2012: # redo year so filename is correct
-        year = year+1
+    if year < 2012:  # redo year so filename is correct
+        year = year + 1
     league = f'volleyball_cev_cup_women'
     get_team_standings(year, driver, league)
     get_team_stats(year, driver, league)
     get_player_stats(year, driver, league)
 
+
 def get_golden_league_men(year, driver):
     try:
-        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1295') # contains table with all years
+        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1295')  # contains table with all years
         link = driver.find_element(By.PARTIAL_LINK_TEXT, f'{year}').get_attribute('href')
         driver.get(link)
         # it's not possible to use the click() function, therefore you have to get the url and use driver.get()
@@ -197,9 +231,10 @@ def get_golden_league_men(year, driver):
     get_team_stats(year, driver, league)
     get_player_stats(year, driver, league)
 
+
 def get_golden_league_women(year, driver):
     try:
-        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1296') # contains table with all years
+        driver.get(f'https://www-old.cev.eu/Competition-Area/History.aspx?ID=1296')  # contains table with all years
         link = driver.find_element(By.PARTIAL_LINK_TEXT, f'{year}').get_attribute('href')
         driver.get(link)
         # it's not possible to use the click() function, therefore you have to get the url and use driver.get()
@@ -211,8 +246,9 @@ def get_golden_league_women(year, driver):
     get_team_stats(year, driver, league)
     get_player_stats(year, driver, league)
 
+
 def get_team_stats(year, driver, league):
-    try: # load data
+    try:  # load data
         driver.find_element(By.PARTIAL_LINK_TEXT, f'Statistics').click()
         time.sleep(3)
         elements = driver.find_elements(By.CLASS_NAME, 'rtsTxt')
@@ -231,8 +267,9 @@ def get_team_stats(year, driver, league):
         print('could not scrape data')
         print(e)
 
+
 def get_team_standings(year, driver, league):
-    try: # load data
+    try:  # load data
         driver.find_element(By.PARTIAL_LINK_TEXT, f'Home').click()
         time.sleep(3)
         tables = driver.find_elements(By.CLASS_NAME, "rgMasterTable")
@@ -246,8 +283,9 @@ def get_team_standings(year, driver, league):
         print('could not scrape data')
         print(e)
 
+
 def get_player_stats(year, driver, league):
-    try: # load data
+    try:  # load data
         driver.find_element(By.PARTIAL_LINK_TEXT, f'Statistics').click()
         time.sleep(3)
         select_element = driver.find_element(By.ID, 'Content_Left_DDL_PlayerRanking')
@@ -265,9 +303,12 @@ def get_player_stats(year, driver, league):
                 html = table.get_attribute("outerHTML")
                 df = pd.read_html(html)[0]
                 df = check_tables(df)
-                filename = f'{league}_{name}_{table_counter}_{year}.csv' # problem with naming
+                filename = f'{league}_{name}_{table_counter}_{year}.csv'  # problem with naming
                 safe_csv(df, filename)
                 table_counter = table_counter + 1
     except Exception as e:
         print('could not scrape data')
         print(e)
+# %%
+
+# %%
