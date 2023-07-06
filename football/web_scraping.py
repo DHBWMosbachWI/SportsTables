628bdf3b502af7d3506026901c94ac85cd48a4f3
Maileen Roll <mai.roll.20@lehre.mosbach.dhbw.de>
2023-07-06 16:23:18 +0200

    football small changes

--- football/web_scraping.py
+++ football/web_scraping.py
@@ -13,40 +13,6 @@ def safe_csv(df, filename):
         print('could not safe csv file')
         print(e)
 
-def web_table_to_dataframe(web_table): # remove?
-    """
-    This function turns a html table into a pandas dataframe. 
-    thead is used as column names of the dataframe and all rows under tbody are going into the dataframe rows.
-    
-    Args:
-        web_table: html table as BeautifulSoup table object
-        
-    Return:
-        df: pandas.DataFrame containing the content of the provided html table
-    """
-    header_row = web_table.find("thead")("tr")[-1]
-
-    ## extract table header
-    table_header = []
-    for col_header in header_row:
-        if col_header.name == "th":
-            table_header.append(col_header.get_text().replace(" ", ""))
-            
-    # extract table content
-    content_rows = web_table.find("tbody")("tr")
-    content_data = []
-    for content_row in content_rows:
-        if len(table_header) != len(content_row):# "table header should be of same length as the table body row"
-            continue
-        row = []
-        for data_cell in content_row:
-            #print(data_cell.get_text())
-            row.append(data_cell.get_text().strip())
-        content_data.append([data_cell.get_text().strip() for data_cell in content_row])
-
-    df = pd.DataFrame(content_data, columns=table_header)
-    return df
-
 def get_player_passing_stats(year:int, driver):
     driver.get(f"https://www.pro-football-reference.com/years/{year}/passing.htm")
     try:
